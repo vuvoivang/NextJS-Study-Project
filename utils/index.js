8dd70884bed8dd44830763d92e0d0f754c8b49fc
Võ Hoàng Vũ <vuprotk21@gmail.com>
2022-03-19 23:30:09 +0700

    handle use-case download description file in product detail + create folder client, services...

---
+++ utils/index.js
@@ -0,0 +1,7 @@
+// export const downloadFile = (id) => {
+//     id = Number(id);
+//     if (id % 2 === 0) {
+//         window.location = "../public/files/EvenProduct.xlsx";
+//     }
+//     else window.location = "../public/files/OddProduct.xlsx";
+// }
