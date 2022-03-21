ea849f7783df82551e8592120e2c096f43e44e10
Võ Hoàng Vũ <vuprotk21@gmail.com>
2022-03-21 14:35:45 +0700

    add https

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production" // chế độ dev hay production
-export const server = dev ? "http://localhost:3000" : "demo-next-project.vercel.app" // where's server?
+export const server = dev ? "http://localhost:3000" : "https://demo-next-project.vercel.app" // where's server?
 // export const server = "http://localhost:3000";
